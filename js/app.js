cd1b902826a12ac743edaf3a1f918fd414cb0917
Spiotrman <seejay3@wp.pl>
2017-03-13 15:49:26 +0100

    navbar

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
-var $document = $(document),
-    $element = $('.navbar'),
-    navbarDefault = 'navbar-default',
-    navbarTransparent = 'navbar-inverse',
-    
-    fadeInDown = 'fadeInDown';
-$document.scroll(function() {
-    if ($document.scrollTop() >= 100) {
-        $element.addClass(navbarTransparent);
-        $element.removeClass(navbarDefault);
-        
-        $element.addClass(fadeInDown);
-        
-    } else {
-        $element.addClass(navbarDefault);
-        $element.removeClass(navbarTransparent);
-        
-        $element.removeClass(fadeInDown);
+$(document).ready(function () {
+
+    var menu = $('.navbar');
+    var navi = $('.navi');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.navbar').addClass('navbar-fixed-top');
+            $('.navi').addClass('navtop');
+        } else {
+            $('.navbar').removeClass('navbar-fixed-top');
+            $('.content').removeClass('navtop');
+        }
+
+
     }
-    
-    
-    
+
+    document.onscroll = scroll;
+
 });
